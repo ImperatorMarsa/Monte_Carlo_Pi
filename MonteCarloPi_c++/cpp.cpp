ca5e9a95207ed660ff1baad80d02a2f4810b0690
ImperatorMarsa <tima.sitnikov@mail.ru>
2018-06-12 07:58:49 +0300

    Add files via upload

---
+++ MonteCarloPi_c++/cpp.cpp
@@ -0,0 +1,47 @@
+#include <omp.h>
+#include <cstdlib>
+#include <iostream>
+#include <math.h>
+#include <ctime>
+
+using namespace std;
+
+int main(){
+
+    #pragma omp parallel
+    cout<<"Hell"<<endl;
+
+
+	int popitok=10E7;
+	int papodaniy=0;
+
+	unsigned int start_time=clock();
+	for(int i=0; i<popitok; i++){
+		double randX=(double)rand()/(double)RAND_MAX;
+		double randY=(double)rand()/(double)RAND_MAX;
+
+		if((pow(randX, 2)+pow(randY, 2))<=1){
+			papodaniy++;
+		}
+	}
+	double pi1=4.0*(double)papodaniy/(double)popitok;
+	unsigned int stop_time=clock();
+	cout<<pi1<<" "<<stop_time-start_time<<endl;
+
+	papodaniy=0;
+	start_time=clock();
+	#pragma omp parallel for
+	for(int i=0; i<popitok; i++){
+		double randX=(double)rand()/(double)RAND_MAX;
+		double randY=(double)rand()/(double)RAND_MAX;
+
+		if((pow(randX, 2)+pow(randY, 2))<=1){
+			papodaniy++;
+		}
+	}
+	double pi2=4.0*(double)papodaniy/(double)popitok;
+	stop_time=clock();
+	cout<<pi2<<" "<<stop_time-start_time<<endl;
+
+	return 0;
+}
