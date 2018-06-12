f275f8df43d3a236fe2178456c8f936f18a6973f
ImperatorMarsa <tima.sitnikov@mail.ru>
2018-06-12 08:18:54 +0300

    Add files via upload

--- MonteCarloPi_c++/cpp.cpp
+++ MonteCarloPi_c++/cpp.cpp
@@ -8,13 +8,14 @@ using namespace std;
 
 int main(){
 
-    #pragma omp parallel
-    cout<<"Hell"<<endl;
+	#pragma omp parallel
+	cout<<"Hell"<<endl;
 
 
 	int popitok=10E7;
 	int papodaniy=0;
 
+	cout<<"Single sream"<<endl;
 	unsigned int start_time=clock();
 	for(int i=0; i<popitok; i++){
 		double randX=(double)rand()/(double)RAND_MAX;
@@ -24,10 +25,11 @@ int main(){
 			papodaniy++;
 		}
 	}
-	double pi1=4.0*(double)papodaniy/(double)popitok;
+	double pi1=4.0*(double)papodaniy/(double)popitok;	
 	unsigned int stop_time=clock();
 	cout<<pi1<<" "<<stop_time-start_time<<endl;
 
+	cout<<"\n\nMulti sream"<<endl;	
 	papodaniy=0;
 	start_time=clock();
 	#pragma omp parallel for
